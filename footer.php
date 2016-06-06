adac04749e843e1af8d6ff39769c8b3c0a2ea050
mishingo <miguel@localstaffingllc.com>
2016-06-06 15:06:15 -0400

    update wp-fe

--- footer.php
+++ footer.php
@@ -64,9 +64,9 @@
 <script   src="https://code.jquery.com/jquery-2.2.4.min.js"   integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="   crossorigin="anonymous"></script>
 <script>
 $( document ).ready(function() {
-    $( "#mobile-menu-bar" ).hide();
+    //$( "#mobile-menu-bar" ).hide();
     $("#mobile-menu-action").click(function(){
-    	console.log("yuck");
+    	
     	$( "#mobile-menu-bar" ).slideToggle();
     	
     });
