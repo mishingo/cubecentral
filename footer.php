fa8b85b4033c3e6c4c7881d8731ffab1eaeb36d2
mishingo <miguel@localstaffingllc.com>
2016-06-16 10:46:27 -0400

    move ga code

--- footer.php
+++ footer.php
@@ -65,29 +65,6 @@
 
 
 
-
-<script>
-    (function (i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] || function () {
-                    (i[r].q = i[r].q || []).push(arguments)
-                }, i[r].l = 1 * new Date();
-        a = s.createElement(o),
-                m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
-        m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
-    ga('create', 'UA-54006220-12', 'auto');
-</script>
-<script>
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-54006220-12']);
-	_gaq.push(['_setDomainName', 'onlineresumebuilders.com']);
-	_gaq.push(['_trackPageview']);
-</script>
-
 <script   src="https://code.jquery.com/jquery-2.2.4.min.js"   integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="   crossorigin="anonymous"></script>
 <script>
 $( document ).ready(function() {
--- header.php
+++ header.php
@@ -24,6 +24,21 @@
 
 <!-- <link rel="stylesheet" href="http://localhost:3000/app/index.css"> -->
 <?php wp_head(); ?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-54006220-12', 'auto');
+  ga('send', 'pageview');
+</script>
+<script>
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-54006220-12']);
+	_gaq.push(['_setDomainName', 'onlineresumebuilders.com']);
+	_gaq.push(['_trackPageview']);
+</script>
 </head>
 
 
